3ec2102c82a4392d44af09befe7ad6321dbd37d1
Oren Levine <olevine@icfj.org>
2018-06-15 10:06:27 -0400

    save daily data to Airtable

--- indextrump.py
+++ indextrump.py
@@ -6,7 +6,10 @@ import time
 from secrets import *
 from urllib import request
 from bs4 import BeautifulSoup
+from airtable import airtable
 
+at = airtable.Airtable(os.environ['airtable_indextrump_base'], os.environ['airtable_apikey'])
+at.get('IndexTrump')
 
 def send_tweet(paper, trumpindex):
     # Twitter requires all requests to use OAuth for authentication
@@ -35,6 +38,10 @@ def send_update(api, txt):
     except Exception as e:
         print(e)
 
+#updates the Airtable record for today with the page & index
+def update_airtable(dbrec, paper, index):
+    at.update('IndexTrump', dbrec, {paper: index})
+
 def get_index():
 
     # pages we track
@@ -47,7 +54,10 @@ def get_index():
                        ("NBC News","https://www.nbcnews.com/", "@nbcnews"),
                        ("CBS News","https://www.cbsnews.com/","@CBSNews"),
                        ("ABC News","https://abcnews.go.com/","@ABC"),
-					   ("The Guardian","https://www.theguardian.com/us","@GuardianUS")]
+                       ("The Guardian","https://www.theguardian.com/us","@GuardianUS")]
+
+    # Create a new record in Airtable
+    dbrec = at.create('IndexTrump', {'Date': now.strftime("%Y-%m-%d")})
 
     for paper in tracking_papers:
         try:
@@ -65,8 +75,11 @@ def get_index():
             #alltrumps = soup.find_all(string="Trump")
             print(len(alltrumps))
 
+            # update the database
+            update_airtable(dbrec, paper[0], len(alltrumps))
+
             # send the tweet!
-            send_tweet(paper, len(alltrumps))
+#            send_tweet(paper, len(alltrumps))
             # and wait a few sec before the next one
             time.sleep(10)
         except Exception as e:
